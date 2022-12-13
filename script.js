5d4ffce8f63217a5fa98e9b4509596ddbecc530f
Muhammad Muhaimin Ahmed Al-Amin <116969641+itsamuhaimin@users.noreply.github.com>
2022-12-14 03:55:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Orbiting Animated Text Splitter
 const text = document.querySelector('.text p');
 text.innerHTML = text.innerHTML.split("").map((char, i) => 
     `<span style="transform: rotate(${i * 14}deg)">${char}</span>`).join("");
+
+// Dark Mode
+document.getElementById('open-menu-btn').addEventListener('click', ()=> {
+    document.getElementById('nav-btn').style.display = 'flex';
+    document.getElementById('close-menu-btn').style.display = 'inline-block';
+    document.getElementById('open-menu-btn').style.display = 'none';
+})
+document.getElementById('close-menu-btn').addEventListener('click', ()=> {
+    document.getElementById('nav-btn').style.display = 'none';
+    document.getElementById('close-menu-btn').style.display = 'none';
+    document.getElementById('open-menu-btn').style.display = 'inline-block';
+})
+
+// Scroll Top
+function home(){
+    window.scrollTo(0, 0);
+}
